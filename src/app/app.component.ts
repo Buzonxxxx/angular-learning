fd9a33ccec910f7cbf3f29047a75cf0cbfd562d5
buzonxxxx <buzonliao@gmail.com>
2024-09-16 09:45:47 +0800

    add component property binding & event binding code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,14 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'angular-learning';
   name = 'Louis';
+  imageUrl = 'https://picsum.photos/id/237/500/500'
+  updatedImageUrl = "https://fastly.picsum.photos/id/331/200/300.jpg?hmac=p5C3371_uSYqznhNsddJ6h1t3gMS35ijqJoWBTuBRIQ"
 
   getName() {
     return this.name;
+  }
+
+  changeImage(e: KeyboardEvent) {
+    this.imageUrl = (e.target as HTMLInputElement).value;
   }
 }
