0de54036bc898c9f4fb2ca76197a8c8b9976dc3c
buzonxxxx <buzonliao@gmail.com>
2024-09-16 09:50:01 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ export class AppComponent {
   title = 'angular-learning';
   name = 'Louis';
   imageUrl = 'https://picsum.photos/id/237/500/500'
-  updatedImageUrl = "https://fastly.picsum.photos/id/331/200/300.jpg?hmac=p5C3371_uSYqznhNsddJ6h1t3gMS35ijqJoWBTuBRIQ"
 
   getName() {
     return this.name;
